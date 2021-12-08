909b2b9d0c88f118e00e23664bfcfca04c96c869
unknown <alielsaeed1121@gmail.com>
2021-12-08 02:12:44 +0200

    getProduct,getProdoctById,getProductBySuppId,add,update,delete Product Serviceس

--- src/app/Components/product/product.module.ts
+++ src/app/Components/product/product.module.ts
@@ -1,5 +1,9 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import { IProduct } from 'src/app/ViewModels/iproduct';
 
 
 
@@ -11,4 +15,48 @@ import { CommonModule } from '@angular/common';
     CommonModule
   ]
 })
-export class ProductModule { }
+export class ProductModule {
+  pro!:Observable<IProduct[]>
+  constructor(private productService:HttpClient) { }
+
+
+  getAllProducts() :Observable<IProduct[]>
+  {
+    return this.productService.get<IProduct[]>(`${environment.ApiUrl}/Product`);
+  }
+
+  getProductByID(ID:number) :Observable<IProduct>
+  {
+    return this.productService.get<IProduct>(`${environment.ApiUrl}/Product/`+ID);
+  }
+
+  getProductBySuppID(SuppID:number) :Observable<IProduct>
+  {
+   return this.productService.get<IProduct>(`${environment.ApiUrl}/product/GetProductBySupplierID/`+SuppID);
+  }
+
+  addProduct(prod:IProduct) :Observable<any>
+  {
+    const httpOptions={
+      headers : new HttpHeaders({
+        'content-type':'application/JSON'
+      })
+    }
+    return this.productService.post(`${environment.ApiUrl}/Product/addProduct`,JSON.stringify(prod),httpOptions);
+  }
+
+  editProduct(prod:IProduct) :Observable<any>
+  {
+    const httpOptions={
+      headers : new HttpHeaders({
+        'content-type':'application/JSON'
+      })
+    }
+    return this.productService.put(`${environment.ApiUrl}/Product/editProduct`,JSON.stringify(prod),httpOptions);
+  }
+
+  deleteProduct(ID:number):void
+  {
+    this.productService.delete(`${environment.ApiUrl}/Product/Delete/`+ID)
+  }
+ }
